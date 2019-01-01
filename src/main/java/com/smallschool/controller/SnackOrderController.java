47c6547d40b9fee898f03d60156066575c0b80ef
niujunbo119@163.com <niujunbo119zz>
2019-01-01 19:41:17 +0800

    commit

--- src/main/java/com/smallschool/controller/SnackOrderController.java
+++ src/main/java/com/smallschool/controller/SnackOrderController.java
@@ -1,14 +1,16 @@
 package com.smallschool.controller;
 
+import com.smallschool.entity.CartEntity;
+import com.smallschool.entity.SnackEntity;
 import com.smallschool.entity.SnackOrderEntity;
 import com.smallschool.entity.UserEntity;
-import com.smallschool.service.ServiceOrderRepository;
-import com.smallschool.service.SnackOrderRepository;
-import com.smallschool.service.UserRepository;
+import com.smallschool.service.*;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,17 +25,30 @@ public class SnackOrderController {
     UserRepository userRepository;
 
     @Autowired
-    SnackOrderRepository snackOrderRepository;
+    CartRepository cartRepository;
+
+    @Autowired
+    SnackRepository snackRepository;
 
     @RequestMapping("/toShopCart")
     public String shopcart(Map map){
 
-        List<UserEntity> user_list = userRepository.findAll();
-        List<SnackOrderEntity> snack_order_list = snackOrderRepository.findAll();
+        UserEntity user_list = userRepository.findById(1).orElse(null);
+        List<CartEntity> cart_list = cartRepository.findAll();
+        List<SnackEntity> snack_list =new ArrayList<>();
+        SnackEntity snackEntity =null;
+        Base64 encoder = new Base64();
+        for (CartEntity cart : cart_list) {
+            snackEntity=snackRepository.findById(cart.getSnackId()).orElse(null);
+            String pic ="data:image/jpeg;base64,"+encoder.encodeBase64String(snackEntity.getSnackPic());
+            snackEntity.setPicString(pic);
+            snack_list.add(snackEntity);
+        }
+
 
         map.put("user_list",user_list);
-        map.put("snack_order_list",snack_order_list);
-
+        map.put("cart_list",cart_list);
+        map.put("snack_list",snack_list);
         return "shopcart";
     }
 
